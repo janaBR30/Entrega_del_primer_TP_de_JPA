b2705246a94dce207711188177201f1edd4c0a29
Janaina Bacin <114409955+janaBR30@users.noreply.github.com>
2023-09-16 11:20:33 -0300

    Update Ejer01Application.java

--- ejer01/src/main/java/com/utn/ejer01/Ejer01Application.java
+++ ejer01/src/main/java/com/utn/ejer01/Ejer01Application.java
@@ -2,18 +2,15 @@ package com.utn.ejer01;
 
 		import com.utn.ejer01.entidades.*;
 		import com.utn.ejer01.enums.Estado;
+		import com.utn.ejer01.enums.FormaDePago;
 		import com.utn.ejer01.enums.TipoEnvio;
-		import com.utn.ejer01.repositorios.ClienteRepository;
-		import com.utn.ejer01.repositorios.DomicilioRepository;
-		import com.utn.ejer01.repositorios.PedidoRepository;
-		import jakarta.annotation.PostConstruct;
+		import com.utn.ejer01.enums.TipoProducto;
+		import com.utn.ejer01.repositorios.*;
 		import org.springframework.beans.factory.annotation.Autowired;
 		import org.springframework.boot.CommandLineRunner;
 		import org.springframework.boot.SpringApplication;
 		import org.springframework.boot.autoconfigure.SpringBootApplication;
-		import org.springframework.context.ConfigurableApplicationContext;
 		import org.springframework.context.annotation.Bean;
-
 		import java.text.SimpleDateFormat;
 		import java.util.Date;
 
@@ -21,11 +18,19 @@ package com.utn.ejer01;
 @SpringBootApplication
 public class Ejer01Application {
 	@Autowired
-	ClienteRepository clienteRepository;
+	public RubroRepository rubroRepository;
 	@Autowired
-	DomicilioRepository domicilioRepository;
+	public ProductoRepository productoRepository;
 	@Autowired
-	PedidoRepository pedidoRepository;
+	public DomicilioRepository domicilioRepository;
+	@Autowired
+	public ClienteRepository clienteRepository;
+	@Autowired
+	public PedidoRepository pedidoRepository;
+	@Autowired
+	public DetallePedidoRepository detallePedidoRepository;
+	@Autowired
+	public FacturaRepository facturaRepository;
 
 
 	public static void main(String[] args) {
@@ -34,76 +39,149 @@ public class Ejer01Application {
 	}
 
 	@Bean
-	CommandLineRunner init(ClienteRepository clienteRepo,DomicilioRepository domicilioRepo, PedidoRepository pedidoRepo) {
-		return args -> {System.out.println("-----------------ESTOY FUNCIONANDO---------");
+	CommandLineRunner init(ClienteRepository clienteRepo, DomicilioRepository domicilioRepo, PedidoRepository pedidoRepo) {
+		return args -> {
+			System.out.println("-----------------ESTOY FUNCIONANDO---------");
 
-			SimpleDateFormat formatoFecha= new SimpleDateFormat("yyyy-MM-dd");
+			SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
 			String fechaString = "2023-09-09";
-			Date fecha= formatoFecha.parse(fechaString);
+			Date fecha = formatoFecha.parse(fechaString);
 
 /*El método builder() se genera automáticamente por Lombok y te permite crear una instancia de Cliente.Builder.
 Luego, puedes encadenar llamadas a los métodos setters generados automáticamente para establecer los valores de los atributos de la clase.
 Finalmente, build() crea la instancia de la clase Cliente con los valores proporcionados.*/
 
+			//crear instancia rubro
+			Rubro rubro1 = Rubro.builder()
+					.denominacion("Comida")
+					.build();
+			//crear instancias de productos
+			Producto producto1 = Producto.builder()
+					.tiempoEstimadoCocina(60)
+					.denominacion("Hamburguesa con Cheddar")
+					.precioVenta(2000.00)
+					.precioCompra(1200.00)
+					.stockActual(50)
+					.stockMin(3)
+					.unidadMedida("unidad1")
+					.receta("receta1")
+					.tipo(TipoProducto.INSUMO)
+					.build();
+			Producto producto2 = Producto.builder()
+					.tiempoEstimadoCocina(60)
+					.denominacion("Hamburguesa con Baccon")
+					.precioVenta(2200.00)
+					.precioCompra(1500.00)
+					.stockActual(32)
+					.stockMin(4)
+					.unidadMedida("unidad2")
+					.receta("receta2")
+					.tipo(TipoProducto.INSUMO)
+					.build();
+			//agregar productos al rubro
+			rubro1.agregarProducto(producto1);
+			rubro1.agregarProducto(producto2);
+			//guardar rubro
+			rubroRepository.save(rubro1);
+			//Crear instancia Cliente
+			Cliente cliente1 = Cliente.builder()
+					.nombre("Frida")
+					.apellido("Kallo")
+					.email("fridakallo@mail")
+					.telefono("telefono1")
+					.build();
+			//Crear instancia domicilio
 			Domicilio domicilio1 = Domicilio.builder()
-					.calle("Calle 1")
-					.numero("501")
-					.localidad("Mendoza")
+					.calle("San Martin")
+					.numero("6538")
+					.localidad("Ciudad")
 					.build();
-
 			Domicilio domicilio2 = Domicilio.builder()
-					.calle("Calle 2")
-					.numero("123")
-					.localidad("Guaymallén")
+					.calle("Epidio")
+					.numero("1223")
+					.localidad("Guaymallen")
+					.build();
+			//agregar domicilios a cliente
+			cliente1.agregarDomicilio(domicilio1);
+			cliente1.agregarDomicilio(domicilio2);
+			//Crear Instancia Detalle Pedido
+			DetallePedido detallePedido1 = DetallePedido.builder()
+					.cantidad(2)
+					.subtotal(4000.00)
+					.build();
+			DetallePedido detallePedido2 = DetallePedido.builder()
+					.cantidad(1)
+					.subtotal(2200.00)
+					.build();
+			DetallePedido detallePedido3 = DetallePedido.builder()
+					.cantidad(3)
+					.subtotal(60000.00)
 					.build();
 
+			//Crear Instancia Pedido
 			Pedido pedido1 = Pedido.builder()
-					.TIPODEENVIO(TipoEnvio.DELIVERY)
-					.ESTADO(Estado.INICIADO)
-					.total(5200.50)
 					.fecha(fecha)
-					.factura(Factura.builder().build())
+					.total(6200.00)
+					.estado(Estado.ENTREGADO)
+					.tipoEnvio(TipoEnvio.DELIVERY)
 					.build();
 			Pedido pedido2 = Pedido.builder()
-					.TIPODEENVIO(TipoEnvio.RETIRA)
-					.ESTADO(Estado.PREPARACION)
-					.total(10000.50)
 					.fecha(fecha)
-					.factura(Factura.builder().build())
+					.total(6000.00)
+					.estado(Estado.ENTREGADO)
+					.tipoEnvio(TipoEnvio.DELIVERY)
 					.build();
-
-			// Crear instancia de Cliente y agregar domicilios
-			Cliente cliente = Cliente.builder()
-					.nombre("Juan")
-					.apellido("Pérez")
-					.telefono("123456")
-					.email("juanperez@gmail.com")
+			//Crear instancias de factura
+			Factura factura1 = Factura.builder()
+					.fecha(fecha)
+					.total(5800.00)
+					.numero(1)
+					.descuento(400.00)
+					.formaDePago(FormaDePago.EFECTIVO)
 					.build();
+			Factura factura2 = Factura.builder()
+					.fecha(fecha)
+					.total(5400.00)
+					.numero(2)
+					.descuento(600.00)
+					.formaDePago(FormaDePago.MERCADOPAGO)
+					.build();
+			//Agregar detalles al pedido
+			pedido1.agregarDetallePedido(detallePedido1);
+			pedido1.agregarDetallePedido(detallePedido2);
+			pedido2.agregarDetallePedido(detallePedido3);
+			//agregar pedidos al cliente
+			cliente1.agregarPedido(pedido1);
+			cliente1.agregarPedido(pedido2);
+			//vincular el detalle pedido con el producto
+			detallePedido1.setProducto(producto1);
+			detallePedido2.setProducto(producto2);
+			detallePedido3.setProducto(producto1);
+			//vincular factura con pedido
+			pedido1.setFactura(factura1);
+			pedido2.setFactura(factura2);
+			//guardar cliente
+			clienteRepository.save(cliente1);
 
-			cliente.agregarDomicilio(domicilio1);
-			cliente.agregarDomicilio(domicilio2);
-			cliente.agregarPedido(pedido1);
-			cliente.agregarPedido(pedido2);
+//recuperar objeto rubro desde la base de datos
+			Rubro rubroRecuperado = rubroRepository.findById(rubro1.getId()).orElse(null);
+			if (rubroRecuperado != null){
+				System.out.println("Denominacion: " + rubroRecuperado.getDenominacion());
+				rubroRecuperado.mostrarProductos();
+			}
+			//recuperar Cliente desde la base de Datos
+			Cliente clienteRecuperado = clienteRepository.findById(cliente1.getId()).orElse(null);
+			if (clienteRecuperado != null){
+				System.out.println("Nombre: " + clienteRecuperado.getNombre());
+				System.out.println("Apellido: " + clienteRecuperado.getApellido());
+				System.out.println("Mail: " + clienteRecuperado.getEmail());
+				System.out.println("Telefono: " + clienteRecuperado.getTelefono());
+				System.out.println("----------------------------------------");
+				clienteRecuperado.mostrarDomicilios();
+				clienteRecuperado.mostrarPedidos();
 
-			// Guardar el objeto CLiente en la base de datos
-			clienteRepository.save(cliente);
-
-
-			// Recuperar el objeto CLiente desde la base de datos
-			Cliente clienteRecuperada = clienteRepository.findById(cliente.getId()).orElse(null);
-
-
-			if (clienteRecuperada != null) {
-				System.out.println("Nombre: " + clienteRecuperada.getNombre());
-				System.out.println("Apellido: " + clienteRecuperada.getApellido());
-				System.out.println("Telefono: " + clienteRecuperada.getTelefono());
-				System.out.println("Email: " + clienteRecuperada.getEmail());
-				clienteRecuperada.mostrarDomicilios();
-				clienteRecuperada.mostrarPedidos();
 			}
 
 		};
-
 	}
-
 }
